ddc66df8b36160cdca71230c1114dbe9856ffa3e
TheMysticalBard <deathlyangel47@gmail.com>
2020-08-05 05:33:11 -0500

    Tweaked chance to catch fish flops and updated fish flops sprite.

--- JonsPlayer.cs
+++ JonsPlayer.cs
@@ -1,9 +1,4 @@
 ﻿using JonsVanities.Items;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -13,7 +8,13 @@ namespace JonsVanities
     {
         public override void CatchFish(Item fishingRod, Item bait, int power, int liquidType, int poolSize, int worldLayer, int questFish, ref int caughtType, ref bool junk)
         {
-            if (poolSize >= 1000 && Main.rand.NextBool(10))
+            //This is equal to the very rare rarity of vanilla Terraria fish.
+            float catchChance = 1 / (2250 / power);
+            if (catchChance > 0.2f)
+                catchChance = 0.2f;
+
+            //If in the oceam (poolSize doesn't go higher than 75 for some reason) and the chance is met, catch the fish flops!
+            if (poolSize >= 75 && player.ZoneBeach && Main.rand.NextFloat() <= catchChance)
             {
                 caughtType = ModContent.ItemType<FishFlops>();
             }
